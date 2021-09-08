007a6cd086a04d544f9729f7e1798c14e1067173
catanielena <catani.elena1@gmail.com>
2021-09-08 11:29:30 +0200

    Fix: string to numb

--- snack-1/main.js
+++ snack-1/main.js
@@ -5,60 +5,60 @@
 var orto = [
     {
         "varietà": "v1",
-        "peso": "100",
-        "lunghezza": "15",
+        "peso": 100,
+        "lunghezza": 15,
     },
     {
         "varietà": "v4",
-        "peso": "200",
-        "lunghezza": "10",
+        "peso": 200,
+        "lunghezza": 10,
     },
     {
         "varietà": "v4",
-        "peso": "120",
-        "lunghezza": "13",
+        "peso": 120,
+        "lunghezza": 13,
     },
     {
         "varietà": "v1",
-        "peso": "130",
-        "lunghezza": "15",
+        "peso": 130,
+        "lunghezza": 15,
     },
     {
         "varietà": "v2",
-        "peso": "50",
-        "lunghezza": "12",
+        "peso": 50,
+        "lunghezza": 12,
     },
     {
         "varietà": "v1",
-        "peso": "100",
-        "lunghezza": "15",
+        "peso": 100,
+        "lunghezza": 15,
     },
     {
         "varietà": "v2",
-        "peso": "55",
-        "lunghezza": "10",
+        "peso": 55,
+        "lunghezza": 10,
     },
     {
         "varietà": "v3",
-        "peso": "125",
-        "lunghezza": "12",
+        "peso": 125,
+        "lunghezza": 12,
     },
     {
         "varietà": "v1",
-        "peso": "143",
-        "lunghezza": "14",
+        "peso": 143,
+        "lunghezza": 14,
     },
     {
         "varietà": "v5",
-        "peso": "110",
-        "lunghezza": "13",
+        "peso": 110,
+        "lunghezza": 13,
     },
     
 ];
 
 var pesoTot = 0;
 for(var i=0; i<orto.length; i++) {
-    pesoTot += parseInt(orto[i].peso);
+    pesoTot += orto[i].peso;
     console.log(orto[i].peso)
 }
 console.log(`pesoTot`, pesoTot);
